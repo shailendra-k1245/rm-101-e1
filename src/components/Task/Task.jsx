c5c1f3a4400b0e47ab21e05ac2a04686caa5826a
Shailendra <91621557+shailendra-k1245@users.noreply.github.com>
2022-06-09 08:55:44 +0530

    task details

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import styles from "./task.module.css";
 import { Counter } from "../Counter/index";
 import { GiTireIronCross } from "react-icons/gi";
+import { deleteTasks } from "../../Redux/action";
+import { useDispatch } from "react-redux";
 
 const Task = (props) => {
   const count = props.data.count;
   const id = props.data.id;
-  // console.log("propsdata", props.data.count);
+  const dispatch = useDispatch();
+
   // NOTE: do not delete `data-testid` key value pair
+
+  const handleClick = () => {
+    // console.log("clicked");
+    dispatch(deleteTasks(id));
+  };
+
   return (
     <li data-testid="task" className={styles.task}>
       <input type="checkbox" data-testid="task-checkbox" />
       <div data-testid="task-text">{props.data.text}</div>
       {/* Counter here */}
       <Counter count={count} id={id} />
-      <button data-testid="task-remove-button">
+      <button data-testid="task-remove-button" onClick={handleClick}>
         <GiTireIronCross />
       </button>
     </li>
--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from "./taskHeader.module.css";
 
 const TaskHeader = () => {
+  const todos = useSelector((store) => store.todos);
+  const totalTask = todos.length;
+  let c = 0;
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].done == false) {
+      c++;
+    }
+  }
+
+  const unCompletedTask = c;
+
   // sample values to be replaced
-  let totalTask = 0;
-  let unCompletedTask = 0;
+  // let totalTask = 0;
+  // let unCompletedTask = 0;
 
   // NOTE: do not delete `data-testid` key value pair
   return (
     <div data-testid="task-header" className={styles.taskHeader}>
       <h1>Todo List</h1>
-      You have <b data-testid="header-remaining-task">{unCompletedTask}</b> of <b data-testid="header-total-task">{totalTask}</b> tasks remaining 
+      You have <b data-testid="header-remaining-task">
+        {unCompletedTask}
+      </b> of <b data-testid="header-total-task">{totalTask}</b> tasks remaining
     </div>
   );
 };
